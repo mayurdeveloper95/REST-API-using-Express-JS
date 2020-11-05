72769e0bb13b75012e95bc5168ce038239883eb4
unknown <you@example.com>
2020-11-05 18:07:11 +0530

    REST API using Express JS

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,9 +1,45 @@
-let Express = require('express');
-let app=new Express();
+let express = require('express');
+let app=express();
+app.use(express.json());
 
-
-app.get((req,res)=>
+let unames=[{
+    id:1,
+    name:"mayur",
+},
 {
-    res.write("welcome");
-})
-app.listen(4000);
+    id:2,
+    name:"rohit",
+},
+{
+    id:3,
+    name:"chinmay"
+}];
+app.get("/uname",(req,res)=>
+{
+res.send(unames);
+});
+
+app.get("/courses",(req,res)=>
+{
+res.send(JSON.stringify(["angular","node","php","html"]));
+});
+
+
+app.get("/una/:id",(req,res)=>
+{
+let usernames=unames.find((data)=>data.id===parseInt(req.params.id));
+if(!usernames){return res.status(404).send({message:"invalid course id"})};
+res.send(usernames);
+});
+
+app.post("/createunames",(req,res)=>
+{
+    let un={
+    id: unames.length+1,
+    name: req.body.name
+    };
+    unames.push(un);
+    res.send(unames);
+});
+
+app.listen(7200,()=>console.log("port connected to 7200"));
