4715a2caae26de44be4252ad42b60fd1c2bf3e3e
unknown <you@example.com>
2020-11-06 16:22:25 +0530

    Api creation using Expressjs

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,6 +1,7 @@
 let express = require('express');
-let app=express();
+let app = express();
 app.use(express.json());
+let Joi = require("joi");
 
 let unames=[{
     id:1,
@@ -14,6 +15,8 @@ let unames=[{
     id:3,
     name:"chinmay"
 }];
+
+//get api
 app.get("/uname",(req,res)=>
 {
 res.send(unames);
@@ -24,7 +27,7 @@ app.get("/courses",(req,res)=>
 res.send(JSON.stringify(["angular","node","php","html"]));
 });
 
-
+//get data by id
 app.get("/una/:id",(req,res)=>
 {
 let usernames=unames.find((data)=>data.id===parseInt(req.params.id));
@@ -32,14 +35,47 @@ if(!usernames){return res.status(404).send({message:"invalid course id"})};
 res.send(usernames);
 });
 
+//creata api
 app.post("/createunames",(req,res)=>
 {
+let schema=Joi.object({
+name:Joi.string().min(4).max(15).required(),
+});
+
+let result=schema.validate(req.body);
+if(result.error){return res.status(404).send(result.error.details[0].message)};
     let un={
     id: unames.length+1,
     name: req.body.name
     };
+
     unames.push(un);
     res.send(unames);
+});
+
+//update data by id
+app.put("/updateunames/:id",(req,res)=>
+{
+let sc=Joi.object({
+name:Joi.string().min(4).max(12).required(),
+});
+let result2=sc.validate(req.body);
+if(result2.error){return res.status(404).send(result2.error.details[0].message)};
+let uu=unames.find((data)=>data.id===parseInt(req.params.id));
+if(!uu){return res.status(404).send({message:"id not found"})};
+uu.name=req.body.name;
+res.send(unames);
+});
+
+
+//delete data by id
+app.delete("/deleteunames/:id",(req,res)=>
+{
+let du=unames.find((data)=>data.id===parseInt(req.params.id));
+if(!du){return res.status(400).send({message:"id not found"})};
+let index=unames.indexOf(du);
+unames.splice(index,1);
+res.send(unames);
 });
 
 app.listen(7200,()=>console.log("port connected to 7200"));
