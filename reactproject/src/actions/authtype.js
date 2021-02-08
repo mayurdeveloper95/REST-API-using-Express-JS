584a518ef70e81a7a7ec10ce16ff3cb99c157c21
Mayur <69307674+mayurdeveloper95@users.noreply.github.com>
2021-02-08 12:59:32 +0530

    Delete reactproject directory

--- reactproject/src/actions/authtype.js
+++
@@ -1,1 +0,0 @@
-export const FETCH_AUTH_DATA="FETCH_AUTH_DATA";
--- reactproject/src/actions/usertype.js
+++
@@ -1,7 +0,0 @@
-export const USER_REGISTER = "USER_REGISTER";
-export const USER_LOGIN = "USER_LOGIN";
-export const LOGGEDIN_USER = "LOGGEDIN_USER";
-export const ERROR = "ERROR";
-export const USER_LOGOUT = "USER_LOGOUT";
-export const GET_USER="GET_USER";
-export const DELETE_USER="DELETE_USER"
